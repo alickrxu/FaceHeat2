3902e0a0b25ed29c5b93dd1e7d78ab76e5746ea5
alickrxu <alickrxu@gmail.com>
2015-03-14 22:13:11 -0700

    ported over, time to fix forehead/cheek and add heat detection method

---
+++ FaceHeat2/ViewController.h
@@ -0,0 +1,30 @@
+//
+//  ViewController.h
+//  FaceHeatIOS
+//
+//  Created by Alick, Daijiro on 2/19/15.
+//  Copyright (c) 2015 Team3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <FLIROneSDK/FLIROneSDK.h>
+
+@interface ViewController : UIViewController <FLIROneSDKImageReceiverDelegate, FLIROneSDKStreamManagerDelegate, FLIROneSDKVideoRendererDelegate, FLIROneSDKImageEditorDelegate>
+
+@property (weak, nonatomic) IBOutlet UIImageView *ycBview;
+@property (weak, nonatomic) IBOutlet UIImageView *thermalView;
+@property (weak, nonatomic) IBOutlet UILabel *faceFeatureLabel;
+@property (strong, nonatomic) CIDetector *facedetector;
+
+@property (weak, nonatomic) IBOutlet UILabel *rightEyeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *leftEyeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *mouthLabel;
+@property (weak, nonatomic) IBOutlet UILabel *originLabel;
+@property (weak, nonatomic) IBOutlet UILabel *frameCountLabel;
+@property (weak, nonatomic) IBOutlet UILabel *connectionLabel;
+
+
+@property (strong, nonatomic) NSArray * faceFeatures;
+@end
+
---
+++ FaceHeat2Tests/FaceHeat2Tests.m
@@ -0,0 +1,40 @@
+//
+//  FaceHeat2Tests.m
+//  FaceHeat2Tests
+//
+//  Created by Alick Xu on 3/14/15.
+//  Copyright (c) 2015 Alick Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface FaceHeat2Tests : XCTestCase
+
+@end
+
+@implementation FaceHeat2Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
